e98c7f6f662a39f80a2ac15fa71b54e10726ded6
Amrit Anand Singh <50056654+amritanandsingh@users.noreply.github.com>
2023-07-26 23:05:34 +0530

    library ,file directory is ready to rock

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use(express.static('./assets'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
