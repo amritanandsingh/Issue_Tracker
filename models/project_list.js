0f4e99a2cc67cc9ed16e733eb4cdb225aed1d3b4
Amrit Anand Singh <50056654+amritanandsingh@users.noreply.github.com>
2023-07-28 23:05:01 +0530

    mongoose , project_list model , connect to db all things are ready

---
+++ models/project_list.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const project_Schema = new mongoose.Schema({
+    name:{
+        type: String,
+        required : true,
+        unique: true
+    },
+    description:{
+        type: String
+    },
+    Author:{
+        type: String,
+        required : true
+    }
+
+}, {
+    timestamps: true
+});
+
+const projectModel = mongoose.model("project",project_Schema);
+module.exports=projectModel;
